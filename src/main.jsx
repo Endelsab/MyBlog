ab7a668650599b3c69e9bc7f941bffa567899423
Endelsab <141985348+Endelsab@users.noreply.github.com>
2024-03-02 18:59:42 +0800

    Your commit message here

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,8 @@ import App from "./App.jsx";
 import "./index.css";
 //import {ThemeProvider} from "@mui/material"
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		
-			<App />
-		
+		<App />
 	</React.StrictMode>,
 );
